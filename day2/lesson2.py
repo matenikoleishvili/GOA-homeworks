8b68ab3cac4e679d029fa8f7bc9480adb16c4cb3
matenikoleishvili <matenikoleishvili409@gmail.com>
2024-02-07 18:30:57 +0400

    first commit

---
+++ day2/lesson2.py
@@ -0,0 +1,2 @@
+print("gavicani gio gagnize chemi razmis lideri,")
+print("pirveli gakvetilidan zaaan kargi shtabewdileba damrcha rogorc maswavlebelze asevel zogadad yvelaze ratqmaunda megobruli garemoa rac zalian kargi jerjerobit didi dro ar maq gatarebuli margram ra droc goash gavatare zalian momewona")
